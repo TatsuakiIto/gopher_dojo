79dc36a672bb793c60be67b3a2d6a37b60c52ff6
TatsuakiIto <akitatsu95.climbing@gmail.com>
2021-11-18 17:22:01 +0900

    feat: ディレクトリを掘る処理

--- main.go
+++ main.go
@@ -4,25 +4,40 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"strings"
 )
 
 func main()  {
+
+	// 引数をパースして引数以下のファイルを読み込めるようにする
 	flag.Parse()
-	fmt.Println(flag.Args())
+	args := flag.Args()
+	dirName := genDirName(args[0])
+
+	readFiles(dirName)
+
+}
+
+// 引数を一つ渡して、ディレクトリ名を返す
+func genDirName(s string) string{
+	dirName := "./" + s
+	return dirName
 }
 
 // ディレクトリ内のファイルを読み取る処理
-func readFiles()  {
-	files, _ := ioutil.ReadDir("./test")
+func readFiles(dir string) {
+	files, _ := ioutil.ReadDir(dir)
 
 	// 本当はエラーハンドリングする
 
 	var names []string
 	for _, file := range files {
-		newName := strings.Replace(file.Name(), ".txt", ".csv", 1)
-		names = append(names, newName)
+		names = append(names, file.Name())
 	}
 
 	fmt.Println(names)
+}
+
+// TODO: fileを渡してjpegの拡張子をpngに変える処理を作る
+func changeExtension() {
+
 }
